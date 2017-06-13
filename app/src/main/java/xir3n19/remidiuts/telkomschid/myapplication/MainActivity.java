4785148b6adbc6c948233d23540d4acfb67c14f0
Mufidatun Nabilah <mufida.nabilah3@gmail.com>
2017-06-13 07:16:54 -0700

    1

---
+++ app/src/main/java/xir3n19/remidiuts/telkomschid/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package xir3n19.remidiuts.telkomschid.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
